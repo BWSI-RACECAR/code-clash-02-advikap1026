1788a2a7029fe8718ac61fb8f54bb93f2a77d6a7
Advika Prashanth <advika.prashanth08@gmail.com>
2023-07-13 09:09:38 -0400

    Update digitdecrypt.py

--- digitdecrypt.py
+++ digitdecrypt.py
@@ -4,10 +4,13 @@ class Solution:
             #return type: int
             
             #TODO: Write code below to returnn an int with the solution to the prompt.
-            x = num%10 
-            y = num%100 
-            sum = x + y 
-            return sum 
+            tens = 10 
+            for i in range(9):
+                x = num%10 
+                y = num%100 
+                sum = x+y
+                return sum 
+                
             pass
  
 def main():
