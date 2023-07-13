8f4753749ec125633c09ace079ba84ed91a6b354
Advika Prashanth <advika.prashanth08@gmail.com>
2023-07-13 09:14:26 -0400

    Update digitdecrypt.py

--- digitdecrypt.py
+++ digitdecrypt.py
@@ -5,12 +5,15 @@ class Solution:
             
             #TODO: Write code below to returnn an int with the solution to the prompt.
             tens = 10 
+            sum = 0
+            arr = [char for char in nums]
             for i in range(9):
-                x = num%10 
-                y = num%100 
-                sum = x+y
+                while arr.len != 1:
+                     for i in range arr.len:
+                        
+                        sum += arr[i]
+                        
                 return sum 
-                
             pass
  
 def main():
