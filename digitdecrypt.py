af4d3b088852118c33c61a254dbcbb1b00c08483
Advika Prashanth <advika.prashanth08@gmail.com>
2023-07-13 09:05:27 -0400

    Create digitdecrypt.py

---
+++ digitdecrypt.py
@@ -0,0 +1,23 @@
+class Solution:    
+    def digitdecrypt(self, num):
+            #type num: int
+            #return type: int
+            
+            #TODO: Write code below to returnn an int with the solution to the prompt.
+            x = num%10 
+            y = num%100 
+            sum = x + y 
+            return sum 
+            pass
+ 
+def main():
+    input1= input()
+    input2 = int(input1)
+
+    
+    tc1 = Solution()
+    ans = tc1.digitdecrypt(input2)
+    print(ans)
+    
+if __name__ == "__main__":
+    main()
